36644aad1fcca2caf2791e3c6311978b7e890ffb
Ankit Kumar <165273813+AnkitKumar-0@users.noreply.github.com>
2025-05-01 22:01:13 +0530

    refactor

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -25,6 +25,7 @@ const Hero = ({movies = []}) => {
                             </Paper>
                         )
                     })
+                    
                 }
             </Carousel>
         </div>
